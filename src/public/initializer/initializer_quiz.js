af8878091f9208944074b6e0e362ece3de19d232
Yuyu Xu <athena.xyy@gmail.com>
2016-03-21 15:10:29 -0400

    fix quiz page validation

--- src/public/initializer/initializer_quiz.js
+++ src/public/initializer/initializer_quiz.js
@@ -2,7 +2,7 @@
 var QuizPageHelper = {
   GatherSubmitData : function() {
     submit_data = [];
-    submit_data.push($('input[name=group0]:checked').val());
+    // submit_data.push($('input[name=group0]:checked').val());
     submit_data.push($('input[name=group1]:checked').val());
     submit_data.push($('input[name=group2]:checked').val());
     submit_data.push($('input[name=group3]:checked').val());
@@ -11,41 +11,43 @@ var QuizPageHelper = {
     return submit_datastr;
   },
 
-  IsSubmitDataValid : function() {
-    return true;
+  IsSubmitDataValid : function(submit_data_str) {
+    // TEST, uncomment
+    // return true;
 
-    var side = $('#player-side').val();
+    submit_data = JSON.parse(submit_data_str);
+    var side = $('#player-role').val();
     if (side == "type-0") {
+      // if (submit_data[0] == null || submit_data[0] != "sam")  return false;
       if (submit_data[0] == null || submit_data[0] != "high")  return false;
       if (submit_data[1] == null || submit_data[1] != "med")  return false;
       if (submit_data[2] == null || submit_data[2] != "low")  return false;
       if (submit_data[3] == null || submit_data[3] != "records")  return false;
-      if (submit_data[4] == null)  return false;
       return true;
     }
   },
 
   LoadSubmitData : function(submit_data_str) {
     submit_data = JSON.parse(submit_data_str);
+    // if (submit_data[0] != null) {
+    //   var $radios = $('input:radio[name=group0]');
+    //   $radios.filter('[value=' + submit_data[0] + ']').prop('checked', true);
+    // }
     if (submit_data[0] != null) {
-      var $radios = $('input:radio[name=group0]');
+      var $radios = $('input:radio[name=group1]');
       $radios.filter('[value=' + submit_data[0] + ']').prop('checked', true);
     }
     if (submit_data[1] != null) {
-      var $radios = $('input:radio[name=group1]');
+      var $radios = $('input:radio[name=group2]');
       $radios.filter('[value=' + submit_data[1] + ']').prop('checked', true);
     }
     if (submit_data[2] != null) {
-      var $radios = $('input:radio[name=group2]');
+      var $radios = $('input:radio[name=group3]');
       $radios.filter('[value=' + submit_data[2] + ']').prop('checked', true);
     }
     if (submit_data[3] != null) {
-      var $radios = $('input:radio[name=group3]');
-      $radios.filter('[value=' + submit_data[3] + ']').prop('checked', true);
-    }
-    if (submit_data[4] != null) {
       var $radios = $('input:radio[name=group4]');
-      $radios.filter('[value=' + submit_data[4] + ']').prop('checked', true);
+      $radios.filter('[value=' + submit_data[3] + ']').prop('checked', true);
     }
   }
 };
@@ -70,13 +72,13 @@ $(document).ready(function() {
   });
   $('#next-stage').click(function() {
     var submit_data_str = QuizPageHelper.GatherSubmitData();
-    Logger.Log('quiz: submit next page ' + submit_data_str);
     $('#quiz_data_prev').val(submit_data_str);
     $('#quiz_data_next').val(submit_data_str);
-    if (QuizPageHelper.IsSubmitDataValid() == true) {
+    if (QuizPageHelper.IsSubmitDataValid(submit_data_str)) {
       $('#next-stage').closest('form').submit();
+      Logger.Log('quiz: submit next page ' + submit_data_str);
     } else {
-      bootbox.alert('Answer is not correct, please retry.');
+      alert('Answer is not correct, please retry.');
     }
   });
 });
