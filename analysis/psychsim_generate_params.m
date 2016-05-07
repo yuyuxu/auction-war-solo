113afc7be83552dfba24636a11c16c075105248a
yuyuxu <athena.xyy@gmail.com>
2016-05-07 12:37:55 -0700

    updated script to generate file instead parameters

--- analysis/psychsim_generate_params.m
+++ analysis/psychsim_generate_params.m
@@ -11,3 +11,31 @@ for wr = 0.01:0.01:0.05
   end
 end
 fclose(fileid);
+
+for i = 1:3
+  fileid = fopen(sprintf('bsub_py%d.bash', i), 'w');
+  fprintf(fileid, '#!/bin/sh\n');
+  fprintf(fileid, '#BSUB -J JOB.py.%d\n', i);
+  fprintf(fileid, '#BSUB -o out_file%d\n', i);
+  fprintf(fileid, '#BSUB -e err_file%d\n', i);
+  fprintf(fileid, '#BSUB -n 8\n');
+  fprintf(fileid, '#BSUB -q ht-10g\n');
+  fprintf(fileid, '#BSUB -cwd /home/xu.yuyu/research/psychsim\n');
+  fprintf(fileid, 'work=/home/xu.yuyu/research/psychsim\n');
+  fprintf(fileid, 'cd $work\n');
+  fprintf(fileid, 'tempfile1=hostlistrun\n');
+  fprintf(fileid, 'tempfile2=hostlist-tcp\n');
+  fprintf(fileid, 'echo $LSB_MCPU_HOSTS > $tempfile1\n');
+  fprintf(fileid, 'declare -a hosts\n');
+  fprintf(fileid, 'read -a hosts < ${tempfile1}\n');
+  fprintf(fileid, 'for ((i=0; i<${#hosts[@]}; i += 2)) ;\n');
+  fprintf(fileid, 'do\n');
+  fprintf(fileid, '  HOST=${hosts[$i]}\n');
+  fprintf(fileid, '  CORE=${hosts[(($i+1))]}\n');
+  fprintf(fileid, '  echo $HOST:SCORE >> $tempfile2\n');
+  fprintf(fileid, 'done\n');
+  fprintf(fileid, 'PYTHONPATH=/home/xu.yuyu/research/psychsim python Yuyu_Model_0429_Dan.py -l 2 -ho 1 -d 1.0 -r 10.0 -sb 0 -db 0 -s2db 0 -d2sb 0 -c 0.5 -wr 0.01 -wa 1 -ws 1 -wfa 1 -wfb 1\n');
+  fprintf(fileid, 'rm $work/$tempfile1\n');
+  fprintf(fileid, 'rm $work/$tempfile2\n');
+  fclose(fileid);
+end
